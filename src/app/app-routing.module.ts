feb17aec94767df123c0cd12ed2a51291efd49d2
devacran <migueeldrg@gmail.com>
2020-10-16 11:45:10 -0500

    Routing implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LayoutComponent } from "./layout/layout.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: LayoutComponent,
+    children: [
+      {
+        path: "",
+        loadChildren: () => import("./home/home.module").then(m => m.HomeModule)
+      },
+      {
+        path: "order",
+        loadChildren: () =>
+          import("./order/order.module").then(m => m.OrderModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
