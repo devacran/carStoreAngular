02a68dd0103feff8b8d329a868df53ca0887ff7d
Miguel Angel de la Rosa Guerrero <migueeldrg@gmail.com>
2022-06-24 22:10:47 -0500

    upgrade chore deps and modify order.component

--- src/app/order/order/order.component.ts
+++ src/app/order/order/order.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from "@angular/core";
-import { Params, ActivatedRoute } from "@angular/router";
-import { ProductsService } from "../../core/services/products.service";
-import { Vehicle } from "../../core/models/vehicle.model";
-import { Order } from "../../core/models/order.model";
-import { MatButtonToggleChange } from "@angular/material/button-toggle";
-import { OrderModalComponent } from "../order-modal/order-modal.component";
-import { MatDialog } from "@angular/material/dialog";
+import { Component, OnInit } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../../core/services/products.service';
+import { Vehicle } from '../../core/models/vehicle.model';
+import { Order } from '../../core/models/order.model';
+import { MatButtonToggleChange } from '@angular/material/button-toggle';
+import { OrderModalComponent } from '../order-modal/order-modal.component';
+import { MatDialog } from '@angular/material/dialog';
 @Component({
-  selector: "app-order",
-  templateUrl: "./order.component.html",
-  styleUrls: ["./order.component.styl"]
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.styl'],
 })
 export class OrderComponent implements OnInit {
   constructor(
@@ -17,6 +17,9 @@ export class OrderComponent implements OnInit {
     private products: ProductsService,
     public dialog: MatDialog
   ) {
+    this.route = route;
+    this.products = products;
+    this.dialog = dialog;
     this.openDialog = this.openDialog.bind(this);
   }
 
@@ -24,46 +27,38 @@ export class OrderComponent implements OnInit {
 
   //this is provisional since there isn't an API
   carImages: Object = {
-    black: "YS_CVT_GrisMetalico_03_0.png",
-    silver: "YS_CVT_Plata_02_0.png",
-    red: "YS_CVT_Rojo_05_0.png",
-    blue: "YS_CVT_AzulMetalico_06_0.png"
+    black: 'YS_CVT_GrisMetalico_03_0.png',
+    silver: 'YS_CVT_Plata_02_0.png',
+    red: 'YS_CVT_Rojo_05_0.png',
+    blue: 'YS_CVT_AzulMetalico_06_0.png',
   };
 
-  selectedColor: string = "black";
-  selectedView: string = "exterior";
+  selectedColor: string = 'black';
+  selectedView: string = 'exterior';
   carImg: string = this.carImages[this.selectedColor];
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params: Params) => {
-      console.log(params.params.id);
       this.data = this.products.getProduct(params.params.id);
     });
-    console.log("data en init", this.data);
   }
   toggleVehicleColor(evn: MatButtonToggleChange): void {
     this.selectedColor = evn.value;
     this.carImg = this.carImages[this.selectedColor];
-    console.log(this.carImg, this.selectedColor);
   }
   toggleView(evn: MatButtonToggleChange) {
-    console.log(evn.value);
     this.selectedView = evn.value;
   }
   openDialog(): void {
     const data: Order = {
       ...this.data,
-      imgSrc: this.carImg
+      imgSrc: this.carImg,
     };
     const dialogRef = this.dialog.open(OrderModalComponent, {
-      width: "500px",
-      data: data
+      width: '500px',
+      data: data,
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      console.log("The dialog was closed");
-      console.log(result);
-      // this.animal = result;
-    });
+    //dialogRef.afterClosed().subscribe((result) => {});
   }
 }
